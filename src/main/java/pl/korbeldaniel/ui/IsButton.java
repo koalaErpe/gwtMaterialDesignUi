c193e6ead407089def63c5aea3e229a640182355
danielkorbel2 <danielkorbel2@danielkorbel2-H87-HD3>
2017-11-23 22:01:17 +0100

    Initial commit

---
+++ src/main/java/pl/korbeldaniel/ui/IsButton.java
@@ -0,0 +1,9 @@
+package pl.korbeldaniel.ui;
+
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
+
+public interface IsButton extends IsWidget {
+	public Widget asWidget();
+
+}
---
+++ src/main/java/pl/korbeldaniel/ui/IsDiv.java
@@ -0,0 +1,5 @@
+package pl.korbeldaniel.ui;
+
+public interface IsDiv {
+	IsDiv getElement();
+}
